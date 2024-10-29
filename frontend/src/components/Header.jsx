d6d4cffea2ed60a132e7d2361db99dd89c316b73
MuminElsayed <master_mumin@hotmail.com>
2024-10-28 22:19:42 -0700

    Adding cart page and logic

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,30 +1,39 @@
-import { Navbar, Nav, Container } from "react-bootstrap";
+import { Navbar, Nav, Container, Badge } from "react-bootstrap";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
-import { LinkContainer } from "react-router-bootstrap"
-import logo from "../assets/logo.png"
+import { LinkContainer } from "react-router-bootstrap";
+import logo from "../assets/logo.png";
+import { useSelector } from "react-redux";
 
 const Header = () => {
+  const { cartItems } = useSelector((state) => state.cart);
+
   return (
     <header>
       <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
         <Container>
           <LinkContainer to="/">
-          <Navbar.Brand>
-          <img src={logo} alt="Ecommerce"/>
-          Ecommerce</Navbar.Brand>
+            <Navbar.Brand>
+              <img src={logo} alt="Ecommerce" />
+              Ecommerce
+            </Navbar.Brand>
           </LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav"></Navbar.Toggle>
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-            <LinkContainer to="/cart">
-              <Nav.Link>
-                <FaShoppingCart /> Cart
-              </Nav.Link>
+              <LinkContainer to="/cart">
+                <Nav.Link>
+                  <FaShoppingCart /> Cart{" "}
+                  {cartItems.length > 0 && (
+                    <Badge pill bg="success">
+                      {cartItems.reduce((a, c) => a + c.qty, 0)}
+                    </Badge>
+                  )}
+                </Nav.Link>
               </LinkContainer>
               <LinkContainer to="/login">
-              <Nav.Link>
-                <FaUser /> Login
-              </Nav.Link>
+                <Nav.Link>
+                  <FaUser /> Login
+                </Nav.Link>
               </LinkContainer>
             </Nav>
           </Navbar.Collapse>
